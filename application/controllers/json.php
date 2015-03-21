8b6760dd6287ff0020a93083b42f76ae825ff030
Avinash <avinash@wohlig.com>
2015-03-21 18:26:33 +0530

    ngo image

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -448,6 +448,175 @@ $orderorder="ASC";
 $data["message"]=$this->chintantable->query($pageno,$maxrow,$orderby,$orderorder,$search,$elements,"FROM `powerforone_project`");
 $this->load->view("json",$data);
 }
+     function getprojectbycategory()
+  {
+        
+     $where="";
+     $category=$this->input->get_post('category');
+     if($category==0)
+     {
+     
+     }
+     else
+     {
+        $where.=" WHERE `powerforone_project`.`category`='$category'";
+     }
+    $elements=array();
+    $elements[0]=new stdClass();
+    $elements[0]->field="`powerforone_project`.`id`";
+    $elements[0]->sort="1";
+    $elements[0]->header="ID";
+    $elements[0]->alias="id";
+
+
+    $elements[1]=new stdClass();
+    $elements[1]->field="`powerforone_project`.`name`";
+    $elements[1]->sort="1";
+    $elements[1]->header="Name";
+    $elements[1]->alias="name";
+
+
+    $elements[2]=new stdClass();
+    $elements[2]->field="`powerforone_project`.`category`";
+    $elements[2]->sort="1";
+    $elements[2]->header="Category";
+    $elements[2]->alias="category";
+
+
+    $elements[3]=new stdClass();
+    $elements[3]->field="`powerforone_project`.`ngo`";
+    $elements[3]->sort="1";
+    $elements[3]->header="NGO";
+    $elements[3]->alias="ngo";
+
+
+    $elements[4]=new stdClass();
+    $elements[4]->field="`powerforone_project`.`advertiser`";
+    $elements[4]->sort="1";
+    $elements[4]->header="Advertiser";
+    $elements[4]->alias="advertiser";
+
+
+    $elements[5]=new stdClass();
+    $elements[5]->field="`powerforone_project`.`json`";
+    $elements[5]->sort="1";
+    $elements[5]->header="Json";
+    $elements[5]->alias="json";
+
+
+    $elements[6]=new stdClass();
+    $elements[6]->field="`powerforone_project`.`like`";
+    $elements[6]->sort="1";
+    $elements[6]->header="Likes";
+    $elements[6]->alias="like";
+
+
+    $elements[7]=new stdClass();
+    $elements[7]->field="`powerforone_project`.`share`";
+    $elements[7]->sort="1";
+    $elements[7]->header="Share";
+    $elements[7]->alias="share";
+
+
+    $elements[8]=new stdClass();
+    $elements[8]->field="`powerforone_project`.`follow`";
+    $elements[8]->sort="1";
+    $elements[8]->header="Follow";
+    $elements[8]->alias="follow";
+
+
+    $elements[9]=new stdClass();
+    $elements[9]->field="`powerforone_project`.`facebook`";
+    $elements[9]->sort="1";
+    $elements[9]->header="Facebook";
+    $elements[9]->alias="facebook";
+
+
+    $elements[10]=new stdClass();
+    $elements[10]->field="`powerforone_project`.`twitter`";
+    $elements[10]->sort="1";
+    $elements[10]->header="Twitter";
+    $elements[10]->alias="twitter";
+
+
+    $elements[11]=new stdClass();
+    $elements[11]->field="`powerforone_project`.`google`";
+    $elements[11]->sort="1";
+    $elements[11]->header="Google";
+    $elements[11]->alias="google";
+
+
+    $elements[12]=new stdClass();
+    $elements[12]->field="`powerforone_project`.`status`";
+    $elements[12]->sort="1";
+    $elements[12]->header="Status";
+    $elements[12]->alias="status";
+
+
+    $elements[13]=new stdClass();
+    $elements[13]->field="`powerforone_project`.`order`";
+    $elements[13]->sort="1";
+    $elements[13]->header="Order";
+    $elements[13]->alias="order";
+
+
+    $elements[14]=new stdClass();
+    $elements[14]->field="`powerforone_project`.`views`";
+    $elements[14]->sort="1";
+    $elements[14]->header="Views";
+    $elements[14]->alias="views";
+
+
+    $elements[15]=new stdClass();
+    $elements[15]->field="`powerforone_project`.`video`";
+    $elements[15]->sort="1";
+    $elements[15]->header="Video";
+    $elements[15]->alias="video";
+
+
+    $elements[16]=new stdClass();
+    $elements[16]->field="`powerforone_ngo`.`name`";
+    $elements[16]->sort="1";
+    $elements[16]->header="ngoname";
+    $elements[16]->alias="ngoname";
+
+
+    $elements[17]=new stdClass();
+    $elements[17]->field="`powerforone_category`.`name`";
+    $elements[17]->sort="1";
+    $elements[17]->header="categoryname";
+    $elements[17]->alias="categoryname";
+
+    $elements[18]=new stdClass();
+    $elements[18]->field="`powerforone_project`.`image`";
+    $elements[18]->sort="1";
+    $elements[18]->header="image";
+    $elements[18]->alias="image";
+
+    $elements[18]=new stdClass();
+    $elements[18]->field="`powerforone_ngo`.`image`";
+    $elements[18]->sort="1";
+    $elements[18]->header="ngoimage";
+    $elements[18]->alias="ngoimage";
+
+    $search=$this->input->get_post("search");
+    $pageno=$this->input->get_post("pageno");
+    $orderby=$this->input->get_post("orderby");
+    $orderorder=$this->input->get_post("orderorder");
+    $maxrow=$this->input->get_post("maxrow");
+    if($maxrow=="")
+    {
+        $maxrow=20;
+    }
+    
+    if($orderby=="")
+    {
+        $orderby="id";
+        $orderorder="ASC";
+    }
+    $data["message"]=$this->chintantable->query($pageno,$maxrow,$orderby,$orderorder,$search,$elements,"FROM `powerforone_project` LEFT OUTER JOIN `powerforone_category` ON `powerforone_project`.`category`=`powerforone_category`.`id` LEFT OUTER JOIN `powerforone_ngo` ON `powerforone_project`.`ngo`=`powerforone_ngo`.`id`",$where);
+    $this->load->view("json",$data);
+  }
 //public function getsingleproject()
 //{
 //$id=$this->input->get_post("id");
