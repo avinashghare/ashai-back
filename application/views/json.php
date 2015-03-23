9753968ca03b44ad93b7099e4c01e130bb1827f5
Avinash <avinash@wohlig.com>
2015-03-23 18:46:24 +0530

    changesssss in json view

--- application/views/json.php
+++ application/views/json.php
@@ -1,13 +1,6 @@
 <?php 
-//header('Content-type: application/javascript');
-//header("Access-Control-Allow-Origin: *");
-
-header('Access-Control-Allow-Origin: *');
-             header("Access-Control-Allow-Credentials: true"); 
-             header('Access-Control-Allow-Headers: X-Requested-With');
-             header('Access-Control-Allow-Headers: Content-Type');
-             header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
-             header('Access-Control-Max-Age: 86400');
+header('Content-type: application/javascript');
+header("Access-Control-Allow-Origin: *");
 
 echo json_encode($message);
 ?>
