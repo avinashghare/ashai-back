aee8bdd0ab092d19d47996fe999e2e551b999129
Avinash <avinash@wohlig.com>
2015-03-26 16:03:38 +0530

    changes

--- application/models/restapi_model.php
+++ application/models/restapi_model.php
@@ -22,34 +22,12 @@ WHERE `projectimage`.`project`='$id'")->result();
     
         $query['similarcauses']=$this->db->query("SELECT `similercauses`.`id`, `similercauses`.`projectid`, `similercauses`.`similarprojectid`,  `powerforone_project`.`name`, `powerforone_project`.`category`, `powerforone_project`.`ngo`, `powerforone_project`.`advertiser`, `powerforone_project`.`json`, `powerforone_project`.`like`, `powerforone_project`.`share`, `powerforone_project`.`follow`, `powerforone_project`.`facebook`, `powerforone_project`.`twitter`, `powerforone_project`.`google`, `powerforone_project`.`status`, `powerforone_project`.`order`, `powerforone_project`.`views`, `powerforone_project`.`video`, `powerforone_project`.`image`, `powerforone_project`.`content`, `powerforone_project`.`contribution`, `powerforone_project`.`times`, `powerforone_project`.`donate`, `powerforone_project`.`tagline`, `powerforone_project`.`video2`, `powerforone_project`.`cardtagline`, `powerforone_project`.`indiandoner`, `powerforone_project`.`foreigndoner`,`powerforone_ngo`.`name`AS `ngoname`,`powerforone_category`.`name`AS `categoryname`,`powerforone_project`.`cardimage` AS `cardimage`
 FROM `similercauses` 
-LEFT OUTER JOIN `powerforone_project` ON `powerforone_project`.`id`= `similercauses`.`projectid`
+LEFT OUTER JOIN `powerforone_project` ON `powerforone_project`.`id`= `similercauses`.`similarprojectid`
 LEFT OUTER JOIN `powerforone_category` ON `powerforone_project`.`category`=`powerforone_category`.`id` 
 LEFT OUTER JOIN `powerforone_ngo` ON `powerforone_project`.`ngo`=`powerforone_ngo`.`id`
 WHERE  `similercauses`.`projectid`='$id' LIMIT 0,3")->result();
     
         return $query;
-    }
-    public function createnewsletter($email)
-    {
-        $ispresent=$this->db->query("SELECT * FROM `newsletter` WHERE `email`='$email'")->result();
-//        print_r($ispresent);
-        
-        $data=array(
-                    "email" => $email
-                   );
-        if(empty($ispresent))
-        {
-            
-            $query=$this->db->insert( "newsletter", $data );
-            $id=$this->db->insert_id();
-            return  $id;
-            
-        }
-        else
-        {
-            return 0;
-        }
-            
     }
     
     public function getstaticpages()
@@ -116,6 +94,29 @@ WHERE  `similercauses`.`projectid`='$id' LIMIT 0,3")->result();
             return  0;
         else
             return  $id;
+    }
+    
+    public function createnewsletter($email)
+    {
+        $ispresent=$this->db->query("SELECT * FROM `newsletter` WHERE `email`='$email'")->result();
+//        print_r($ispresent);
+        
+        $data=array(
+                    "email" => $email
+                   );
+        if(empty($ispresent))
+        {
+            
+            $query=$this->db->insert( "newsletter", $data );
+            $id=$this->db->insert_id();
+            return  $id;
+            
+        }
+        else
+        {
+            return 0;
+        }
+            
     }
 }
 ?>
