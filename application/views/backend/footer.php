209dce428bafc5b67949b882aa6c7643eaee3bb1
Chintan Shah <chintan@wohlig.com>
2015-04-02 11:16:13 +0530

    change in tinymce

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -80,7 +80,19 @@
 
   </script>
   
-  <script>tinymce.init({selector:'textarea.tinymce'});</script>
+  <script>
+  
+  tinymce.init({
+    selector: "textarea.tinymce",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
+  
+  </script>
 
 </body>
 </html>
